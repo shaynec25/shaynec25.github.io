f4b516745f6e38f06d0195d418bce5136970d191
Shayne <shayne.chang@zencher.com>
2023-03-23 08:19:49 +0800

    update: vocabbulry-card

--- demos/vocabulry-card/flutter_service_worker.js
+++ demos/vocabulry-card/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "db4c9e69c4d8a4f3372c15ef768d06ad",
-"index.html": "8c6afdd14e76caaf237a86a8511d7761",
-"/": "8c6afdd14e76caaf237a86a8511d7761",
-"main.dart.js": "efcc2e2e1659c7202c116b3fbe6bee03",
+"index.html": "15a99adc76a53aee5a57f37656dbc293",
+"/": "15a99adc76a53aee5a57f37656dbc293",
+"main.dart.js": "6f6583437179a5d78e0e9184c67be5fb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
